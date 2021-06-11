43b0ff7e6fc5e7ebccb466b94b8f2bb31ade4e10
Piotr <piotr5.12.13@gmail.com>
2021-06-11 19:25:06 +0200

    init

---
+++ src/main/java/com/piter/bets/league/eurobets/EurobetsApplication.java
@@ -0,0 +1,13 @@
+package com.piter.bets.league.eurobets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurobetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurobetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piter/bets/league/eurobets/EurobetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piter.bets.league.eurobets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurobetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
