9649f647bfaf6c2f1d87585defd2514080557255
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-08-15 20:02:12 +0200

    Add actuator info

---
+++ match-api/src/main/java/com/piter/match/api/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.piter.match.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    return http.authorizeExchange()
+        .pathMatchers("/actuator/**", "/match/**").permitAll()
+        .anyExchange().authenticated()
+        .and().build();
+  }
+}
