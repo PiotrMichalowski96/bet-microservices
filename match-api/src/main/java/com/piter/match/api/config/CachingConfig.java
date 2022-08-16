03ea52edd2b5f4fbdd269c944b696f299bd7e5a0
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-08-16 17:01:57 +0200

    Fix cache - set expiry time

--- match-api/src/main/java/com/piter/match/api/config/CachingConfig.java
+++ match-api/src/main/java/com/piter/match/api/config/CachingConfig.java
@@ -1,8 +1,11 @@
 package com.piter.match.api.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +13,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CachingConfig {
 
+  @Value("${cache.expiry-in-hours}")
+  private Integer expiryTimeInHours;
+
   @Bean
-  public CacheManager cacheManager() {
-    return new ConcurrentMapCacheManager("matches");
+  public Caffeine caffeine() {
+    return Caffeine.newBuilder().expireAfterWrite(expiryTimeInHours, TimeUnit.HOURS);
+  }
+
+  @Bean
+  public CacheManager cacheManager(Caffeine caffeine) {
+    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+    caffeineCacheManager.setCaffeine(caffeine);
+    return caffeineCacheManager;
   }
 }
