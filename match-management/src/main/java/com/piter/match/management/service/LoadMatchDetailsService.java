acf0c4ebb37eeb8b6c41c23ded7a14a419c3aea6
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-09-13 12:00:22 +0200

    Calling Match API web service (WIP)

--- match-management/src/main/java/com/piter/match/management/service/LoadMatchDetailsService.java
+++ match-management/src/main/java/com/piter/match/management/service/LoadMatchDetailsService.java
@@ -4,14 +4,38 @@ import com.piter.match.management.domain.Match;
 import com.piter.match.management.domain.MatchResult;
 import com.piter.match.management.domain.MatchRound;
 import com.piter.match.management.model.view.MatchDashboardDetails;
+import com.piter.match.management.rest.HandledMatchService;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoadMatchDetailsService {
 
+  private static final String ORDER_BY_MATCH_TIME = "match-time";
+
+  @Value("${mock.enabled:true}")
+  private final Boolean mockEnabled;
+  private final HandledMatchService handledMatchService;
+
   public MatchDashboardDetails loadMatchDetails() {
+    if (mockEnabled) {
+      return mockDashboardDetails();
+    }
+
+    List<Match> matches = handledMatchService.getMatchList(ORDER_BY_MATCH_TIME);
+    return MatchDashboardDetails.builder()
+        .currentPage(1)
+        .totalPages(1)
+        .matches(matches)
+        .loadTime(LocalDateTime.now())
+        .build();
+  }
+
+  private MatchDashboardDetails mockDashboardDetails() {
     List<Match> matches = List.of(
         Match.builder()
             .id(1L)
@@ -38,7 +62,6 @@ public class LoadMatchDetailsService {
             .round(new MatchRound("LaLiga round 30", LocalDateTime.of(2022, 2, 14, 21, 0, 0)))
             .build()
     );
-
     return MatchDashboardDetails.builder()
         .currentPage(1)
         .totalPages(10)
