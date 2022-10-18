ec86b73cf3bd1ef909a1e7e6320c1791e8aecf7c
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-10-18 19:38:34 +0200

    Add header and footer

--- bet-frontend/src/app/app.module.ts
+++ bet-frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer/footer.component';
+import { HeaderComponent } from './shared/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
