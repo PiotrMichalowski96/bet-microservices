c46a00a68f518fda6534aa2c59403199619c3d1b
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-10-19 21:13:47 +0200

    update API security config to fix CORS error in frontend app

--- match-api/src/main/java/com/piter/match/api/config/SecurityConfig.java
+++ match-api/src/main/java/com/piter/match/api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.piter.match.api.config;
 
 import com.piter.api.commons.security.KeycloakReactiveTokenInstrospector;
+import java.util.List;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,9 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.introspection.NimbusReactiveOpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.introspection.ReactiveOpaqueTokenIntrospector;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @EnableWebFluxSecurity
 @Configuration
@@ -49,11 +53,23 @@ public class SecurityConfig {
   public SecurityWebFilterChain noSecurityWebFilterChain(ServerHttpSecurity http) {
     return http
         .csrf().disable()
-        .cors().disable()
-        .headers().frameOptions().disable()
-        .and()
         .authorizeExchange()
         .pathMatchers("/actuator/**", "/matches/**", "/swagger-doc/**").permitAll()
         .and().build();
+  }
+
+  //TODO: for development of front end Angular app
+  @Bean
+  CorsWebFilter corsWebFilter() {
+    var corsConfig = new CorsConfiguration();
+    corsConfig.setAllowCredentials(true);
+    corsConfig.setAllowedOrigins(List.of("http://localhost:4201"));
+    corsConfig.addAllowedMethod("*");
+    corsConfig.addAllowedHeader("*");
+
+    var source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+
+    return new CorsWebFilter(source);
   }
 }
