13147db66fea97479aba716dfb290617bac08e7b
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-11-03 21:26:49 +0100

    Init config server project

---
+++ bet-config-server/src/main/java/com/piter/bet/config/server/BetConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.piter.bet.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetConfigServerApplication.class, args);
+	}
+
+}
