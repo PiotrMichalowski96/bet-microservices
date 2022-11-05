b3e3852b62b25c42efde730ade57ff6fef09f98b
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-11-05 13:28:27 +0100

    Update cloud config server

--- bet-config-server/src/main/java/com/piter/bet/config/server/BetConfigServerApplication.java
+++ bet-config-server/src/main/java/com/piter/bet/config/server/BetConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.piter.bet.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class BetConfigServerApplication {
 
