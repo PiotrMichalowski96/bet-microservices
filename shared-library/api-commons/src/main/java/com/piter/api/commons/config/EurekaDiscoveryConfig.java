d9872c527748bab687ab7f3c2b1e4aaf4dfbbc8e
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-11-06 14:39:24 +0100

    Refactoring discovery profile

--- shared-library/api-commons/src/main/java/com/piter/api/commons/config/EurekaDiscoveryConfig.java
+++ shared-library/api-commons/src/main/java/com/piter/api/commons/config/EurekaDiscoveryConfig.java
@@ -2,9 +2,7 @@ package com.piter.api.commons.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("local-discovery")
 @EnableDiscoveryClient
 @Configuration
 public class EurekaDiscoveryConfig {
