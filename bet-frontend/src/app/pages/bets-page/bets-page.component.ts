7d1a79b57a2b750ac6507b018ad37f2c3f9569c3
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-11-18 19:49:38 +0100

    Fix carousel

--- bet-frontend/src/app/pages/bets-page/bets-page.component.ts
+++ bet-frontend/src/app/pages/bets-page/bets-page.component.ts
@@ -9,10 +9,29 @@ import {BetsService} from "../../services/bets.service";
 })
 export class BetsPageComponent implements OnInit {
 
+  responsiveOptions;
+
   myOwnBets: Bet[] = [];
   allBets: Bet[] = [];
 
   constructor(private betsService: BetsService) {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
   ngOnInit(): void {
