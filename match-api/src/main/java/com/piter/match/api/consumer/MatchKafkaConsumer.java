a05931045ed22da122617edcc438c7938ea8687e
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-11-28 20:26:28 +0100

    Block saving in database to ensure order

--- match-api/src/main/java/com/piter/match/api/consumer/MatchKafkaConsumer.java
+++ match-api/src/main/java/com/piter/match/api/consumer/MatchKafkaConsumer.java
@@ -3,6 +3,7 @@ package com.piter.match.api.consumer;
 import com.piter.api.commons.domain.Match;
 import com.piter.match.api.exception.MatchKafkaException;
 import com.piter.match.api.repository.MatchRepository;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class MatchKafkaConsumer {
+
+  private static final Duration DB_TIMEOUT = Duration.ofSeconds(1);
 
   private final MatchRepository matchRepository;
   private final Map<MatchEventType, Consumer<Message<?>>> eventTypeProcessor;
@@ -36,16 +39,23 @@ public class MatchKafkaConsumer {
   }
 
   private void saveMatch(Message<?> matchMessage) {
+    logReceivedMessage(matchMessage);
     Match match = (Match) matchMessage.getPayload();
-    matchRepository.save(match).subscribe(
-        savedMatch -> logger.debug("Updated match: {}", savedMatch)
-    );
+    matchRepository.save(match).block(DB_TIMEOUT);
+  }
+
+  private void logReceivedMessage(Message<?> matchMessage) {
+    Optional.ofNullable(matchMessage.getHeaders().get(KafkaHeaders.RECEIVED_MESSAGE_KEY, Long.class))
+        .ifPresent(key -> {
+          Match match = (Match) matchMessage.getPayload();
+          logger.debug("Received match to save. Key: {} match: {}", key, match);
+        });
   }
 
   private void deleteMatch(Message<?> tombstoneMessage) {
     Long id = Optional.ofNullable(tombstoneMessage.getHeaders().get(KafkaHeaders.RECEIVED_MESSAGE_KEY, Long.class))
         .orElseThrow(() -> new MatchKafkaException("Delete event does not have Kafka key ID"));
-    matchRepository.deleteById(id).subscribe();
+    matchRepository.deleteById(id).block(DB_TIMEOUT);
     logger.debug("Deleted match id: {}", id);
   }
 }
