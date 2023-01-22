677dcd2cd3a078eeb09ab03c10f417e5bb5a15d1
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2023-01-22 17:52:29 +0100

    Add Embedded MongoDB test group

--- match-api/src/test/java/com/piter/match/api/service/SequenceGeneratorServiceTest.java
+++ match-api/src/test/java/com/piter/match/api/service/SequenceGeneratorServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.piter.api.commons.domain.Match;
 import com.piter.match.api.config.MatchApiTestConfig;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,6 +18,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+@Tag("EmbeddedMongoDBTest")
 @DataMongoTest
 @ActiveProfiles("TEST")
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
