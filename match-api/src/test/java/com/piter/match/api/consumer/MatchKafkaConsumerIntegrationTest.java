c4aa878ab8b01e14473bb6c3eb86218409e1abf8
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2023-04-04 20:03:28 +0200

    [FEATURE] Add kafka consumer tests

---
+++ match-api/src/test/java/com/piter/match/api/consumer/MatchKafkaConsumerIntegrationTest.java
@@ -0,0 +1,90 @@
+package com.piter.match.api.consumer;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.piter.api.commons.domain.Match;
+import com.piter.api.commons.domain.MatchResult;
+import com.piter.api.commons.domain.MatchRound;
+import com.piter.match.api.config.MatchApiTestConfig;
+import com.piter.match.api.repository.MatchRepository;
+import java.time.LocalDateTime;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ThrowingRunnable;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Tag("EmbeddedMongoDBTest")
+@DataMongoTest
+@ActiveProfiles("TEST")
+@ExtendWith(SpringExtension.class)
+@Import(MatchApiTestConfig.class)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+class MatchKafkaConsumerIntegrationTest {
+
+  private static final int TIMEOUT_IN_SECONDS = 20;
+  private static final int POLL_INTERVAL_IN_SECONDS = 2;
+
+  @Autowired
+  private MatchKafkaConsumer matchKafkaConsumer;
+
+  @Autowired
+  private MatchRepository matchRepository;
+
+  @Test
+  void shouldSaveMatch() {
+    //given
+    var id = 99L;
+    var match = Match.builder()
+        .id(id)
+        .homeTeam("FC Barcelona")
+        .awayTeam("Real Madrid")
+        .startTime(LocalDateTime.of(2022, 2, 17, 21, 0, 0))
+        .result(new MatchResult(4, 0))
+        .round(new MatchRound("LaLiga round 30", LocalDateTime.of(2022, 2, 14, 21, 0, 0)))
+        .build();
+
+    matchRepository.save(match).block();
+
+    var updatedMatch = Match.builder()
+        .id(match.getId())
+        .homeTeam(match.getHomeTeam())
+        .awayTeam(match.getAwayTeam())
+        .startTime(match.getStartTime())
+        .result(new MatchResult(4, 2))
+        .round(match.getRound())
+        .build();
+
+    var updatedMatchMessage = MessageBuilder.withPayload(updatedMatch)
+        .setHeader(KafkaHeaders.RECEIVED_MESSAGE_KEY, id)
+        .build();
+
+    //when
+    matchKafkaConsumer.matches().accept(updatedMatchMessage);
+
+    //then
+    assertAsync(() -> assertSavedMatchById(updatedMatch));
+  }
+
+  private void assertAsync(ThrowingRunnable assertion) {
+    Awaitility.await()
+        .atMost(TIMEOUT_IN_SECONDS, SECONDS)
+        .pollInterval(POLL_INTERVAL_IN_SECONDS, SECONDS)
+        .untilAsserted(assertion);
+  }
+
+  private void assertSavedMatchById(Match expectedMatch) {
+    var savedMatch = matchRepository.findById(expectedMatch.getId()).block();
+    assertThat(savedMatch).isEqualTo(expectedMatch);
+  }
+}
--- match-api/src/test/java/com/piter/match/api/consumer/MatchKafkaConsumerTest.java
+++ match-api/src/test/java/com/piter/match/api/consumer/MatchKafkaConsumerTest.java
@@ -1,90 +1,86 @@
 package com.piter.match.api.consumer;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.piter.api.commons.domain.Match;
-import com.piter.api.commons.domain.MatchResult;
-import com.piter.api.commons.domain.MatchRound;
-import com.piter.match.api.config.MatchApiTestConfig;
 import com.piter.match.api.repository.MatchRepository;
-import java.time.LocalDateTime;
-import org.awaitility.Awaitility;
-import org.awaitility.core.ThrowingRunnable;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.integration.support.MessageBuilder;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.kafka.support.KafkaNull;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import reactor.core.publisher.Mono;
 
-@Tag("EmbeddedMongoDBTest")
-@DataMongoTest
-@ActiveProfiles("TEST")
-@ExtendWith(SpringExtension.class)
-@Import(MatchApiTestConfig.class)
-@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@ExtendWith(MockitoExtension.class)
 class MatchKafkaConsumerTest {
 
-  private static final int TIMEOUT_IN_SECONDS = 20;
-  private static final int POLL_INTERVAL_IN_SECONDS = 2;
+  @Mock
+  private MatchRepository matchRepository;
 
-  @Autowired
+  @Captor
+  private ArgumentCaptor<Match> matchArgumentCaptor;
+
   private MatchKafkaConsumer matchKafkaConsumer;
 
-  @Autowired
-  private MatchRepository matchRepository;
+  @BeforeEach
+  void init() {
+    matchKafkaConsumer = new MatchKafkaConsumer(matchRepository);
+  }
 
   @Test
   void shouldSaveMatch() {
     //given
-    var id = 99L;
     var match = Match.builder()
-        .id(id)
-        .homeTeam("FC Barcelona")
-        .awayTeam("Real Madrid")
-        .startTime(LocalDateTime.of(2022, 2, 17, 21, 0, 0))
-        .result(new MatchResult(4, 0))
-        .round(new MatchRound("LaLiga round 30", LocalDateTime.of(2022, 2, 14, 21, 0, 0)))
+        .id(123L)
         .build();
 
-    matchRepository.save(match).block();
-
-    var updatedMatch = Match.builder()
-        .id(match.getId())
-        .homeTeam(match.getHomeTeam())
-        .awayTeam(match.getAwayTeam())
-        .startTime(match.getStartTime())
-        .result(new MatchResult(4, 2))
-        .round(match.getRound())
+    var matchMessage = MessageBuilder
+        .withPayload(match)
         .build();
 
-    var updatedMatchMessage = MessageBuilder.withPayload(updatedMatch)
-        .setHeader(KafkaHeaders.RECEIVED_MESSAGE_KEY, id)
-        .build();
+    mockSave(match);
 
     //when
-    matchKafkaConsumer.matches().accept(updatedMatchMessage);
+    matchKafkaConsumer.matches().accept(matchMessage);
 
     //then
-    assertAsync(() -> assertSavedMatchById(updatedMatch));
+    verify(matchRepository).save(matchArgumentCaptor.capture());
+    Match savedMatch = matchArgumentCaptor.getValue();
+    assertThat(savedMatch).isEqualTo(match);
+  }
+  
+  @Test
+  void shouldDeleteMatch() {
+    //given
+    var messageKey = 123L;
+    Message tombstoneMessage = org.springframework.integration.support.MessageBuilder
+        .withPayload(KafkaNull.INSTANCE)
+        .setHeader(KafkaHeaders.RECEIVED_MESSAGE_KEY, messageKey)
+        .build();
+
+    mockDelete();
+
+    //when
+    matchKafkaConsumer.matches().accept(tombstoneMessage);
+
+    //then
+    verify(matchRepository).deleteById(messageKey);
   }
 
-  private void assertAsync(ThrowingRunnable assertion) {
-    Awaitility.await()
-        .atMost(TIMEOUT_IN_SECONDS, SECONDS)
-        .pollInterval(POLL_INTERVAL_IN_SECONDS, SECONDS)
-        .untilAsserted(assertion);
+  private void mockSave(Match match) {
+    when(matchRepository.save(match)).thenReturn(Mono.just(match));
   }
 
-  private void assertSavedMatchById(Match expectedMatch) {
-    var savedMatch = matchRepository.findById(expectedMatch.getId()).block();
-    assertThat(savedMatch).isEqualTo(expectedMatch);
+  private void mockDelete() {
+    when(matchRepository.deleteById(anyLong())).thenReturn(Mono.empty());
   }
 }
