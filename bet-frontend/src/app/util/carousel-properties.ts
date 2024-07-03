30141d68bdd244a1116cabac574916a61c85db95
Piotr Michalowski <piotr5.12.13@gmail.com>
2024-07-03 17:38:05 +0200

    [FEATURE] increase number of items in carousel

--- bet-frontend/src/app/util/carousel-properties.ts
+++ bet-frontend/src/app/util/carousel-properties.ts
@@ -1,5 +1,20 @@
 export const RESPONSIVE_CAROUSEL_OPTIONS = [
   {
+    breakpoint: '3840px',
+    numVisible: 10,
+    numScroll: 10
+  },
+  {
+    breakpoint: '3440px',
+    numVisible: 9,
+    numScroll: 9
+  },
+  {
+    breakpoint: '1920px',
+    numVisible: 5,
+    numScroll: 5
+  },
+  {
     breakpoint: '1024px',
     numVisible: 3,
     numScroll: 3
